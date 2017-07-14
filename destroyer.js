45258dcf87797db71a989771c6772346801e5c63
Hernan Mendez <hernan21112001@gmail.com>
2017-07-13 21:01:35 -0400

    Done

--- destroyer.js
+++ destroyer.js
@@ -6,9 +6,11 @@ const configs = require('./TDSettings')
 
 var T = new Twit(configs)
 
-T.get('search/tweets', { q: 'from:mr_mc_BOT' }, function(err, data, response) {
+T.get('search/tweets', { q: 'from:mr_mc_BOT' }, (err,data)=>{
   data = data.statuses
   for(let i = 0; i<data.length; i++){
-    console.log(data[i].id)
+    T.post('statuses/destroy/:id', { id: data[i].id_str }, err=>{
+      if(err) console.log('ERROR with: '+data[i].id +'   '+ err)
+    })
   }
 })
