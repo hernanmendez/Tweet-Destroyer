596b7aa1b9f2f89d5064d7b4c8416a09953b1122
Hernan Mendez <hernan21112001@gmail.com>
2017-07-13 20:57:05 -0400

    getting the Tweet ID's from the account I created (mr_mc_BOT)

--- destroyer.js
+++ destroyer.js
@@ -6,7 +6,9 @@ const configs = require('./TDSettings')
 
 var T = new Twit(configs)
 
-    T.post('statuses/update',{status:"test"},(err)=>{
-    if(err) console.log('ERROR')
-    else console.log('Succesful Post!')
-    })
+T.get('search/tweets', { q: 'from:mr_mc_BOT' }, function(err, data, response) {
+  data = data.statuses
+  for(let i = 0; i<data.length; i++){
+    console.log(data[i].id)
+  }
+})
